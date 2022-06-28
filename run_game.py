7e6e339cb9daa09d530f3b8819f4f9049e5da8df
YukkyHub <yukky.bryan@gmail.com>
2022-06-28 10:24:46 +0200

    run game with learned weights

--- run_game.py
+++ run_game.py
@@ -3,7 +3,12 @@ from game.SpaceInvaders import SpaceInvaders
 from controller.keyboard import KeyboardController
 from controller.random_agent import RandomAgent
 from controller.dqn_agent import DQNAgent
+from classes import epsilon_profile
+from classes import networks
 import sys
+import torch
+import time
+import pygame
 
 def format_state(state):
     player_x = round(state[0])
@@ -13,35 +18,51 @@ def format_state(state):
 
 def main(mode):
 
-    game = SpaceInvaders(display=False)
+    game = SpaceInvaders(display=True)
     
     n_episodes = 200
     max_steps = 50
     gamma = 0.9
     alpha = 0.2
-    epsilon = 0.8
+    eps_profile = epsilon_profile.EpsilonProfile(1.0, 0.05)
+    final_exploration_episode = 480
 
+    #DQN Hyperparameters
+    batch_size = 128
+    replay_memory_size = 1000
+    target_update_frequency = 2
+    tau = 1.0
+    
+    n_inputs = len(game.get_state())
+    
     if mode == 'keyboard':
         agent = KeyboardController()
     elif mode == 'random':
         agent = RandomAgent(game.na)
     elif mode == 'dqn':
-        agent = DQNAgent(game.na)
-        agent.learn(n_episodes, max_steps, gamma, alpha, epsilon)
+        model = networks.MLP(n_inputs, game.na)
+        weights = torch.load("trained_0.4762")
+        model.load_state_dict(weights)
+        agent = DQNAgent(game, model, eps_profile, gamma, alpha, replay_memory_size, batch_size, target_update_frequency, tau, final_exploration_episode)
     else:
         print('Unknown agent. Starting as random agent.')
         agent = RandomAgent(game.na)
  
     state = game.reset()
-    while True:
+    score = 0
+    step = 0
+    game_over = False
+    while not game_over:
+        pygame.event.get()
         action = agent.select_action(state)
         state, reward, is_done = game.step(action)
-        sleep(0.0001)
-        
-        format_state(state)
+        score += reward
+        step += 1
+        print(f"Step {step} | Score {score}") if reward > 0 else None
+        game_over = True if is_done else False
 
 if __name__ == '__main__' :
     if (len(sys.argv) > 1):
         main(sys.argv[1])
     else:
-        main('random')
+        main('dqn')
